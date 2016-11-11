f84e873f518557d2fe59707772c1df3d20d5bdc8
rajrajsahil <rajsahildumaribhui@gmail.com>
2016-11-11 14:25:26 +0530

    basic nav pages

--- about.php
+++ about.php
@@ -3,27 +3,11 @@ include "navheader.php";
 ?>
  <div id="intro" style="width:calc(100% - 40px - 5px - 50px);">
  		<h3 style="font-family:champagne;font-size:28px; text-align: center; ">About</h3>
-          				Spring Fest proudly presently Labyrinth 6.0. Labyrinth is an Online Mind Maze. 
+          				Spring Fest proudly presently Labyrinth 7.0. Labyrinth is an Online Mind Maze. 
           				<br>Answers are pretty simple but the way they are hidden makes the jest of the game. Answer can be found to the puzzles
           				presented in this venture by properly matching the clues and hints given at various places in the webpage.
           				Clues can be kept anywhere in the page like an image, url etc.
-        <!--  <div id="levelLeft">
-             <div id="levelHead">
-                 <h1>Level 1</h1>
-             </div>
-             <p>Description</p>
-         </div>
-         <div id="levelRight">
-             <img src="../images/gravity-detached.jpg">
-         </div>
-         <div id="answer">
-             <div class="buttonCont">
-                <input id='level-answer' type='text' class="inputType level-answer" placeholder="  Answer.." >
-            </div>
-            <div class="buttonCont submitAns">
-                <input type="button" class="button level-answerbutton" id='level-answerbutton' value="SUBMIT">
-            </div>
-         </div> -->
+      
 
       </div>
 <?php
